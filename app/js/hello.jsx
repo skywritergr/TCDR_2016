3ec49c93ff531cc68f1dda2d60a32efa2cf3d8c0
George Stefanis <info@georgestefanis.com>
2016-12-02 13:03:57 +0000

    Fixing doctype

--- app/js/hello.jsx
+++ app/js/hello.jsx
@@ -1,16 +1,18 @@
 export default class Hello extends React.Component {
-    constructor(props){
-        super(props);
-        this.state = { data : [] };
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: []
+    };
+  }
 
-    componentDidMount(){
-        // Do Ajax requests here or anything you need to instantiate your state
-    }
+  componentDidMount() {
+    // Do Ajax requests here or anything you need to instantiate your state
+  }
 
-    render(){
-        return (
-          <h1>{this.props.message}</h1>
-        );
-    }
+  render() {
+    return (
+      <h1>{this.props.message}</h1>
+    );
+  }
 }
